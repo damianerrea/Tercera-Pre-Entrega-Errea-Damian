146a31e4c040279dd60da0d610dd42c585af6840
unknown <damianerrea2@hotmail.com>
2023-04-09 02:59:17 -0300

    Faltan detalles

--- AppCoder/forms.py
+++ AppCoder/forms.py
@@ -1,28 +1,14 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Post
+from .models import Post, Mensajes
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
+from django.db import models
 
-class CursoFormulario(forms.Form):
-    curso = forms.CharField()
-    camada = forms.IntegerField()
-
-class ProfesorFormulario(forms.Form):
+class ClienteFormulario(forms.Form):
     nombre=forms.CharField(max_length=30)
     apellido=forms.CharField(max_length=30)
     email = forms.EmailField()
-    profesion = forms.CharField(max_length=30)
-
-class EstudianteFormulario(forms.Form):
-    nombre=forms.CharField(max_length=30)
-    apellido=forms.CharField(max_length=30)
-    email = forms.EmailField()
-class EntregableFormulario(forms.Form):
-    nombre=forms.CharField(max_length=30)
-    fechaDeEntrega=forms.DateField(label="Fecha")
-    entregado = forms.BooleanField(label="¿Fue entregado?")
-
 class UserRegisterForm(UserCreationForm):
     email= forms.EmailField()
     password1=forms.CharField(label='Contraseña', widget=forms.PasswordInput)
@@ -32,13 +18,12 @@ class UserRegisterForm(UserCreationForm):
         fields= ['username', 'email', 'password1', 'password2']
         help_texts={k:"" for k in fields}
 
-class NotasFormulario(forms.Form):
-    nombre_alumno=forms.CharField(max_length=30)
-    apellido_alumno=forms.CharField(max_length=30)
-    fecha_entrega = forms.DateField()
-    nota = forms.DecimalField(max_digits=2, decimal_places=1)
-
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'image']
+        fields = ['title', 'resumen', 'content', 'image']
+
+class MensajeForm(forms.ModelForm):
+    class Meta:
+        model= Mensajes
+        fields=['content']
